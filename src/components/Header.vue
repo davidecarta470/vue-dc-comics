7412bc93f3f7f2ebe4ae36e045cd4f8414921308
davidecarta470 <cartadavi@gmail.com>
2021-11-24 15:24:06 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+   <div>
+     <div>
+       <img src="" alt="">
+     </div>
+     <div class="menu-right">
+        <ul>
+          <li v-for="(item,index) in menuList" :key="index">{{item}}</li>     
+        </ul>
+      </div> 
+   </div>
+</template>
+     
+
+
+<script>
+
+export default {
+  name:'Header',
+  data(){
+    return{
+      menuList:[
+       'CHARACTERS',
+       'COMICS',
+       'MOVIES',
+       'TV',
+       'GAMES',
+       'COLLECTIBLES',
+       'VIDEO',
+       'FANS',
+       'NEWS',
+       'SHOPS',
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+.menu-right{
+  width:60%;
+  font-size:10px;
+}
+ul{
+  display:flex;
+  justify-content: space-around;
+  li{
+    list-style: none;
+    &:hover{
+      color:#0288f9
+    }
+  }
+}
+
+</style>
